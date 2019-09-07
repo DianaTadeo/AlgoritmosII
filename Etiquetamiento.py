a71bf74e2ab559300f31f3a78aaa16e33b799779
lilium <lilium@becarios.tonejito.info>
2019-09-06 22:39:00 -0500

    Documentacion de Etiquetamiento.py

--- Etiquetamiento.py
+++ Etiquetamiento.py
@@ -1,15 +1,13 @@
 '''
-Este programa implementa 2 tipos de representacion de una grafica:
-	-Matricial
-	-En Listas
-Tambien implementa el algoritmo de ford-fulkerson para obtener el
+Este programa implementa la representacion de una grafica en forma Matricial
+Tambien implementa el algoritmo de Etiquetamiento para obtener el
 flujo maximo de una red.
 
 Autor: Diana Tadeo G.
 '''
 import random
-import networkx as nx
-import matplotlib.pyplot as plt
+import networkx as nx #*******Si no se desea mostrar Grafica, se borra*******
+import matplotlib.pyplot as plt#*******Si no se desea mostrar Grafica, se borra*******
 
 '''
 Clase que reprresenta una Grafica dirigida con atributo
@@ -95,18 +93,21 @@ class GraphMatrix:
 			if val[2]<0:
 				edges.append((n2,n1, val[2]*(-1)))
 		return edges
-
+	'''
+	Funcion que implementa el etiquetado de una red
+	calculando las distancias.
+	t: El nodo final (pozo) de la red.
+	'''
 	def etiqueta(self, t):
 		mod=self.graphRes
-		self.distance[t]=0
-		dist=self.distance.copy()
-		totnodos=self.getNodes()
-		nodos=[t]
+		self.distance[t]=0 #La distancia de t=0
+		dist=self.distance.copy() #Se obtienen las distancias de la red que inicialmente estan en -1
+		nodos=[t]#Se guardan los nodos a visitar en una lista
 		val=1
 		copy=nodos.copy()
-		while len(nodos)>0:
-			fin=nodos[-1]
-			actual=nodos.pop(0)
+		while len(nodos)>0:#Se comienzan a recorrer los nodos a visitar
+			fin=nodos[-1]#guardo el ultimo nodo que va a tener la distancia actual
+			actual=nodos.pop(0)#Nodo visitado
 			aux=[]
 			for i in range(len(mod[actual])): #por cada elemento en la fila
 				if mod[actual][i]<0 and actual!=i: #si hay arista al nodo actual desde ese elemento
@@ -115,96 +116,106 @@ class GraphMatrix:
 						if copy.count(i)==0:#Si ese elemento no existe actualmente
 							nodos.append(i)#lo agregamos a la lista de nodo
 							copy.append(i)
-
-			if actual == fin:
+			if actual == fin:#Si llego al ultimo nodo que debe tener esa distancia, aumento el valor
 				val+=1
-
-		self.distance=dist.copy()
+		self.distance=dist.copy()#Al terminar actualizo las distancias de la red
 	
+	'''
+	Funcion que implementa la subrutina 'avanza(i)' del
+	algoritmo de etiquetamiento
+	i: nodo activo
+	j: nodo siguiente
+	path: ruta que se lleva hasta el momento y reemplaza
+		  la funcion pred(i)
+	'''
 	def avanza(self,i,j,path):
 		path.append(j)
 		return j
-
-	def retrocede(self, i,j, path):
-		dist_i=0
+	
+	'''
+	Funcion que implementa la subrutina 'retrocede(i)' del
+	algoritmo de etiquetamiento
+	i: nodo activo
+	j: nodo siguiente(para saber si se puede seguir por ese camino despues)
+	path: ruta que se lleva hasta el momento y reemplaza
+		  la funcion pred(i)
+	'''
+	def retrocede(self, i,j):
 		dist_minx=self.distance[i]
+		#Revisamos el minimo de las distancias
 		dist_minx= self.distance[j] if self.distance[j] < dist_minx else dist_minx
-		self.distance[i]=dist_minx+1
+		self.distance[i]=dist_minx+1#reemplazamos la distancia de i
 
-
+	'''
+	Implementacion del algoritmo de etiquetamiento
+	s: Nodo inicial
+	t: Nodo final
+	'''
 	def laveling_algorithm(self,s, t):
-		self.etiqueta(t)
+		self.etiqueta(t)# Etiquetamos los nodos obteniendo las distancias
 		print("\nLas distancias iniciales \'nodo: distancia\'\n")
 		print(self.distance)
 		i=s
 		path=[s]
 		paro=t+1
-		maxflow=0
-		
-			
+		maxflow=0 #Para guardar el flujo maximo
 		deltaMx=0
 		
 		while (self.distance[s]<paro or deltaMx==maxflow) and self.distance[s]>0: #while d(s)<n do
-			maxflow=0
+			flow_rest=0 #Calculamos si aun se puede enviar flujo desde ese nodo
 			for j in self.graphRes[i]:
-				maxflow+=j if j>0 else 0
-			if maxflow<=0:
+				flow_rest+=j if j>0 else 0
+			if flow_rest<=0:
 				print('\n* Se detiene porque ya no se puede enviar mas flujo por Node(%d)'%i)
 				print('y no hay mas caminos*\n')
 				break
-			#print('Entra al while con i: %d'%i)
-			#print('path salida')
-			#print(path)
-			#print('Val d= %d  val paro= %d distance[i]: %d'%(self.distance[s],paro, self.distance[i]))
 			dist_i=self.distance[i]
 			dist_x=0
-			for j in range(len(self.graphRes[i])):
-				#print('j es: %d' %j)
-				if self.graphRes[i][j]>0:
-					dist_x=self.distance[j]# Si
-					#print('El val es %d - distance[j]: %d' %(self.graphRes[i][j], dist_x))
-					if dist_i==(dist_x+1):   # Tiene un arco admisible
-						#print('dist_i= %d - dist_x=%d -  self.graphRes[i][j]= %d'%(dist_i,dist_x, self.graphRes[i][j]))
-						i=self.avanza(i,j,path)# avanza(i)
-						#print('Tengo mi i= %d'%i)
-						if i==t: #if i=t then
+			for j in range(len(self.graphRes[i])):#Recorremos los posibles vecinos del nodo i
+				if self.graphRes[i][j]>0: #Si hay arista que sale de i -> j
+					dist_x=self.distance[j]		# Si
+					if dist_i==(dist_x+1):   	# Tiene un arco admisible
+						i=self.avanza(i,j,path)	# avanza(i)
+						if i==t: 				#if i=t then
 							print('La ruta elegida es: ')
 							print(path)
-							#print('Llego al fin')
-							#////inicia aumenta(i)
+							
+							#---------inicia aumenta(i)--------------
 							cap=[]
-							#print('Calculo delta...')
-							for i in range(len(path)-1):
-								#print('delta_actual[%d][%d]=%d'%(path[i],path[i+1],self.graphRes[i][i+1]))
+							# Obtengo las capacidades de las aristas entre
+							# los elementos de la ruta
+							for i in range(len(path)-1): 
 								cap.append(self.graphRes[path[i]][path[i+1]])
-							#print(cap)
-							flow=min(cap) #delta
+							flow=min(cap) # obtengo delta
+							# Cambio las capacidades en la residual
 							for elem in range(len(path)-1):
 								cant=self.graphRes[path[elem]][path[elem+1]]
 								self.graphRes[path[elem]][path[elem+1]]=cant-flow
 								self.graphRes[path[elem+1]][path[elem]]=(cant*-1)+flow
-								#print('CANT: %d, delta: %d, ij: %d, ji:%d'%(cant,flow,self.graphRes[path[elem]][path[elem+1]],self.graphRes[path[elem+1]][path[elem]]))
 							print('\nResidual Actual')
 							for index in range(len(self.graphRes)):
 								print(self.graphRes[index])
 							path=[s]
-							#//////acaba aumenta(i)
+							#----------acaba aumenta(i)---------------
+							maxflow+=flow
 							i=s
 						else:
 							break
 					else:
-						self.retrocede(i,j, path)#retrocede(i)
+						self.retrocede(i,j)#retrocede(i)
 			for j in self.graphRes[s]:
 				deltaMx+=j if j>0 else 0
 						
 		print('\n===RESIDUAL FINAL====')
 		for elem in self.graphRes:
 			print(elem)
+		print('\nFlujo Maximo: %d'%maxflow)
 	
 '''
 Funcion que dibuja la grafica dirigida con atributos
 que se le pase como argumento
 '''
+#*******Si no se desea mostrar Grafica, se borra*******
 def drawGraphic(graph):
 	G = nx.DiGraph()
 
@@ -215,7 +226,7 @@ def drawGraphic(graph):
 	pos=nx.spring_layout(G)
 	nx.draw_networkx_nodes(G,pos,
 						nodelist=G.nodes,
-						node_color='#ffa987',
+						node_color='#ffa555',
 						node_size=500,
 						alpha=0.8)
 
@@ -229,16 +240,15 @@ def drawGraphic(graph):
 	plt.show()		
 
 print('\n------------------------------------------------------------------\n')
-print('Obtener el maximo valor de flujo solo esta disponible para la')
-print('representacion de matriz, pero se pueden visualizar ambas')
-print('representaciones.')
+print('			ALGORITMO DE ETIQUETADO')
 print('\n------------------------------------------------------------------\n')
 nodes=int(input('Ingrese la cantidad de nodos: '))
 maxim=int(input('Ingresela cangtidad maxima de capacidad para las aristas: '))
-print('Ingrese la representacion que desea visualizar.')
+grafica=str(input('Desea mostrar la red de forma gr[afica?[S/N] (Default N)'))#****Si no se desea mostrar grafica, se borra
 print('\n*--Representacion--*\n')
 g = GraphMatrix(nodes,maxim) 
 s = 0; t=g.order-1
 g.laveling_algorithm(s,t)
-#drawGraphic(g)
-#print ("El flujo maximo es %d " % g.FordFulkerson(s, t)) 
+#********Si no se desea mostrar Grafica, se borra********
+if grafica=='S':
+	drawGraphic(g)
